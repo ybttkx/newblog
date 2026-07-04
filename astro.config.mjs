059056e430e3b111ea52966a4a2add39f0181ea8
ybttkx <ybnbttkx@163.com>
2026-07-04 16:21:14 +0800

    chore: change blog domain to blog.ybovo.com and add canonical link tag for SEO

--- astro.config.mjs
+++ astro.config.mjs
@@ -26,7 +26,7 @@ import { pluginCustomCopyButton } from "./src/plugins/expressive-code/custom-cop
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://blog.yib.lol/",
+	site: "https://blog.ybovo.com/",
 	base: "/",
 	trailingSlash: "always",
 	integrations: [
